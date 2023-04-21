1bf6596336813686b6d2f48f498be7ed975029fc
erredablio <ricwagner1@gmail.com>
2023-04-21 16:31:00 -0300

    Ajustes na estrutura

--- core/models.py
+++ core/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    email = models.EmailField(null=True, blank=True, max_length=254)
     data = models.DateField(null=True, blank=True)
     escolhido = models.BooleanField(default=False)
 
--- core/views.py
+++ core/views.py
@@ -2,31 +2,48 @@ from django.shortcuts import redirect, render
 from datetime import datetime, timedelta
 from .models import Pessoa
 import random
-import operator
 
 # Create your views here.
 
 def home(request):
-    pessoas = Pessoa.objects.filter(escolhido=True).order_by('data')
-    proximo = Pessoa.objects.filter(escolhido=True).order_by('data')[:1]
+    pessoas = Pessoa.objects.filter(status = True, escolhido = True).order_by('data')
+    atual = Pessoa.objects.filter(status = True, escolhido = True, data__exact=datetime.now()).order_by('data')[:1]
+    proximo = Pessoa.objects.filter(status = True, escolhido = True, data__gt=datetime.now()).order_by('data')[:1]
+    controle_gerador = Pessoa.objects.filter(status = True, escolhido = False).count()
 
-    context = {'pessoas': pessoas, 'proximo': proximo}
+    context = {'pessoas': pessoas, 'atual': atual, 'proximo': proximo, 'controle_gerador': controle_gerador}
     return render(request, 'home.html', context)
 
 def sortear(request):
     i = 0
     x = 7    
-    qtd = Pessoa.objects.count()
-    for i in range(qtd):
-        ninguem = Pessoa.objects.filter(escolhido = False)
-        escolhido = random.choice(ninguem).pk
-        Pessoa.objects.filter(pk=escolhido).update(escolhido = True, data = datetime.now() + timedelta(days=x))    
-        x += 7
-        i += 1    
+    qtd = Pessoa.objects.filter(status = True).count()
+
+    if Pessoa.objects.filter(status = True, escolhido = True, data__lt=datetime.now()).order_by('-data')[:1]:
+        escolhido = Pessoa.objects.filter(status = True, escolhido = True, data__lt=datetime.now()).first().pk
+        maior_data = Pessoa.objects.filter(status = True, escolhido = True).order_by('-data').first().data
+
+        Pessoa.objects.filter(pk = escolhido).update(data = maior_data + timedelta(days=x))
+
+    if Pessoa.objects.filter(status = True, escolhido = True, data=datetime.now()).order_by('-data')[:1]:
+        escolhido = Pessoa.objects.filter(status = True, escolhido = True, data=datetime.now()).first().pk
+        maior_data = Pessoa.objects.filter(statusp = True, escolhido = True).order_by('-data').first().data
+
+        Pessoa.objects.filter(pk = escolhido).update(data = maior_data + timedelta(days=x))
+    
+    if Pessoa.objects.filter(status = True, escolhido = False).count() > 0:
+        i = 0
+        x = 7
+        for i in range(qtd):
+            ninguem = Pessoa.objects.filter(status = True, escolhido = False)
+            escolhido = random.choice(ninguem).pk
+            Pessoa.objects.filter(pk=escolhido).update(status = True, escolhido = True, data = datetime.now() + timedelta(days=x))    
+            x += 7
+            i += 1
     return redirect('../')
 
 
 def limpar(request):
-    Pessoa.objects.filter(escolhido=True).update(escolhido=False)
+    Pessoa.objects.filter(escolhido = True).update(escolhido = False, data = None)
 
     return redirect('../')
